b59eb17889752e9a1054997630ae0581147e7fa2
fabifrank <info@fabifrank.com>
2017-12-02 10:46:47 +0100

    slack notification unit test for constants (success)

--- src/com/grooji/SlackNotification.groovy
+++ src/com/grooji/SlackNotification.groovy
@@ -1,7 +1,19 @@
 package com.grooji;
 
 class SlackNotification {
-  public static buildMessage(action, appName, targetEnv, build, log, recentStage) {
+  public static String BUILD_START = 'Build Start';
+  public static String BUILD_SUCCESS = 'Build SUCCESS';
+  public static String BUILD_ERROR = 'Build ERROR';
+  public static String APPLY_START = 'Apply Start'; // e.g. Kubernets Apply
+  public static String APPLY_SUCCESS = 'Apply SUCCESS';
+  public static String APPLY_ERROR = 'Apply ERROR';
+  public static String DEPLOY_STATUS = 'Deploy Status';
+  public static String DEPLOY_SUCCESS = 'Deploy SUCCESS';
+  public static String DEPLOY_ERROR = 'Deploy ERROR';
+  public static String TEST_SUCCESS = 'Test SUCCESS';
+  public static String TEST_ERROR = 'Test ERROR';
+
+  public static buildMessage(String action, String appName, String targetEnv, String build, String log, String recentStage) {
     String msg = appName;
     if (targetEnv.length() > 0) {
       msg += '\nEnv: ' + targetEnv
--- test/com/grooji/SlackNotification/unit/SlackNotificationSpec.groovy
+++ test/com/grooji/SlackNotification/unit/SlackNotificationSpec.groovy
@@ -5,6 +5,21 @@ import groovy.time.*;
 import spock.lang.*;
 
 class SlackNotificationSpec extends Specification {
+  def 'Constants have correct values'() {
+    expect:
+      com.grooji.SlackNotification.BUILD_START == 'Build Start';
+      com.grooji.SlackNotification.BUILD_SUCCESS == 'Build SUCCESS';
+      com.grooji.SlackNotification.BUILD_ERROR == 'Build ERROR';
+      com.grooji.SlackNotification.APPLY_START == 'Apply Start';
+      com.grooji.SlackNotification.APPLY_SUCCESS == 'Apply SUCCESS';
+      com.grooji.SlackNotification.APPLY_ERROR == 'Apply ERROR';
+      com.grooji.SlackNotification.DEPLOY_STATUS == 'Deploy Status';
+      com.grooji.SlackNotification.DEPLOY_SUCCESS == 'Deploy SUCCESS';
+      com.grooji.SlackNotification.DEPLOY_ERROR == 'Deploy ERROR';
+      com.grooji.SlackNotification.TEST_SUCCESS == 'Test SUCCESS';
+      com.grooji.SlackNotification.TEST_ERROR == 'Test ERROR';
+  }
+
   def 'Build slack messages'() {
     given:
       def action = 'Push artifact to server';
