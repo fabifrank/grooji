a5be7830731548de21798d187b9fa9164af45ad6
fabifrank <info@fabifrank.com>
2017-12-09 13:56:29 +0100

    fix retreiving pod status from recent pod

--- vars/getRecentPodStatus.groovy
+++ vars/getRecentPodStatus.groovy
@@ -2,7 +2,7 @@
  * Get the status of a recent pod
  */
 def call(String podPrefix, String namespace) {
-  def podStatus = sh(returnStdout: true, script: "kubectl describe po ${podPrefix} -n ${namespace} | grep -Eo 'Status' | cut -d: -f2 | tr -d '[:space:]'")
+  def podStatus = sh(returnStdout: true, script: "kubectl describe po ${podPrefix} -n ${namespace} | grep -Eo 'Status:.*' | cut -d: -f2 | tr -d '[:space:]'")
   podStatus = podStatus.trim();
   return podStatus
 }
