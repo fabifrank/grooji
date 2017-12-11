2358c9af519d768274042a21d80376b07b688344
fabifrank <info@fabifrank.com>
2017-12-11 10:01:17 +0100

    add annotation to setRecentStep

--- vars/setRecentStep.groovy
+++ vars/setRecentStep.groovy
@@ -1,3 +1,9 @@
+/**
+ * This method updates the recent step on the main pipeline to be accessed from different methods within the pipeline
+ * Some examples:
+ * - mock functions to track execution of pipeline
+ * - log execution to slack etc..
+ */
 def call(String name) {
   recentStep = name;
 }
